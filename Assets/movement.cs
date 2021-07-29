aa023e0dc3b41ceaec16409117961081703b0c24
jkh5039 <borm3403@naver.com>
2021-07-29 11:35:22 +0900

    start_00

---
+++ Assets/movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Vector3 nowPos;
+    public Vector3 prevPos;
+
+    //마우스 컨트롤 X,Y
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }   
+}
