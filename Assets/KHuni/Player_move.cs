4eea732ad1b3d3880974c3702ec183d461a0764d
jkh5039 <borm3403@naver.com>
2021-07-30 16:14:58 +0900

    blank_c#_delete_n_create

--- Assets/KHuni/Player_move.cs
+++ Assets/KHuni/Player_move.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Player_move : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +13,6 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         
-    }   
+        
+    }
 }
