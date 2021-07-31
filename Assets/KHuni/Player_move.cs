f83fb2827fae656fcbc96e0cb13d7016cc4fe3cf
jkh5039 <50535559+jkh5039@users.noreply.github.com>
2021-07-31 19:12:57 +0900

    .

--- Assets/KHuni/Player_move.cs
+++ Assets/KHuni/Player_move.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Player_move : MonoBehaviour
 {
+    //Camera  게임오브젝트
+    public GameObject camObj;
+    //Camera 컴포넌트
+    Camera cam;
+    //충돌하고 싶지않은 Layer
+    public LayerMask layer;
+
+    bool isPlayerClick;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = camObj.GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //화면 마우스 좌표에서 발사되는 Ray를 만든다.
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Physics.Raycast(ray, out hitInfo, 1000, layer))
+            {
+                isPlayerClick = true;
+            }
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            isPlayerClick = false;
+        }
+
+        if (isPlayerClick)
+        {
+            //Ray발사!!
+
+            if (Physics.Raycast(ray, out hitInfo, 1000, ~layer))
+            {
+                transform.position = hitInfo.point;
+            }
+        }
     }
 }
--- Assets/KHuni/Player_snow.cs
+++ Assets/KHuni/Player_snow.cs
@@ -20,17 +20,32 @@ public class Player_snow : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1")==true) //왼쪽마우스 누르면
         {
-
+            
             //마우스를 뗴기전까지 누른 지속시간을 잰다
+            while (Input.GetButton("Fire1") == true)
+            {
+                float ClickTime = Time.fixedDeltaTime;
+            }
             //ClickTime = Time.fixedDeltaTime; ??????
 
-            //클릭버튼 누른 시간을 변수 x로 가져온다
+            //클릭버튼 누른 시간을 변수 clicktime으로 가져온다
             //만약 클릭시간이 x초 초과된다면 x초로 처리한다 
-            // eg(기본사거리Y+ 가중치w * 시간)
-            //몇초 후에 눈덩이가 destroy가된다 (누른시간이 길면길수록 거리가 멀다)
+            if (ClickTime > 3)
+            {
+                ClickTime = 3;
+            }
 
             GameObject snow = Instantiate(snowFactory); //눈덩이를 만든다.
             snow.transform.position = snowPos.transform.position; //만든 눈덩이를 팔에다 둔다
+            // eg(기본사거리Y+ 가중치w * 시간)
+            float DestroyTime = 1+ 0.6f*ClickTime;
+            //몇초 후에 눈덩이가 destroy가된다 (누른시간이 길면길수록 거리가 멀다)
+            Destroy(snow, DestroyTime);
+            
+            
+
+            //GameObject snow = Instantiate(snowFactory); //눈덩이를 만든다.
+            //snow.transform.position = snowPos.transform.position; //만든 눈덩이를 팔에다 둔다
         }
     }
 }
