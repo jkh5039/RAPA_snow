9a86cdb066f699224ac572635974dced07d98f41
jkh5039 <borm3403@naver.com>
2021-08-02 15:49:31 +0900

    0802_발표전.

--- Assets/KHuni/Player_snow.cs
+++ Assets/KHuni/Player_snow.cs
@@ -18,29 +18,29 @@ public class Player_snow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")==true) //���ʸ��콺 ������
+        if (Input.GetButtonDown("Fire1")==true) //발사버튼클릭
         {
 
-            //���콺�� ���������� ���� ���ӽð��� ����
-            while (Input.GetButtonUp("Fire1")==true)
+            //클릭시간에비례한 사거리증가
+            while (Input.GetButtonUp("Fire1") == true)
             {
                 ClickTime = Time.deltaTime;
             }
-            //ClickTime = Time.fixedDeltaTime; ??????
+            ////ClickTime = Time.fixedDeltaTime; ??????
 
-            //Ŭ����ư ���� �ð��� ���� clicktime���� �����´�
-            //���� Ŭ���ð��� x�� �ʰ��ȴٸ� x�ʷ� ó���Ѵ�
+            //클릭시간이 3초보다 길면 3초로 처리
             if (ClickTime > 3)
             {
                 ClickTime = 3;
             }
 
-            GameObject snow = Instantiate(snowFactory); //�����̸� ������.
+            GameObject snow = Instantiate(snowFactory); //공장에서 눈덩이만듬
             snow.transform.position = snowPos.transform.position; //���� �����̸� �ȿ��� �д�
-            // eg(�⺻���Ÿ�Y+ ����ġw * �ð�)
-            float DestroyTime = 1+ 0.6f*ClickTime;
-            //���� �Ŀ� �����̰� destroy���ȴ� (�����ð��� ���������� �Ÿ��� �ִ�)
+            
+            float DestroyTime = 2+ 0.6f*ClickTime;
+            //일정시간 지난후 snow를 파괴한다
             Destroy(snow, DestroyTime);
+            ClickTime = 0;
 
 
 
